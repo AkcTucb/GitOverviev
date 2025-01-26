18fcc4d08c51b8509f691ef50f91302260f44425
Vladislav Kolesov <AkcTucb1@gmail.com>
2025-01-26 15:30:26 +0300

    Add new java.file

---
+++ src/main/java/net/proselyte/gitoverviev/BaseEntity.java
@@ -0,0 +1,24 @@
+package net.proselyte.gitoverviev;
+
+public class BaseEntity {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+
+    public boolean isNew(){
+        return (this.id == 0);
+    }
+}
